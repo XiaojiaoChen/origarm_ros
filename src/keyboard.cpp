667b7a3a80b4218f6795c0efc89480d8b20b7993
lijing94123 <lijing94123@github.com>
2020-05-15 11:05:26 +0800

    keyboard test on raspberry pi

--- src/keyboard.cpp
+++ src/keyboard.cpp
@@ -1,7 +1,7 @@
 #include <ros/ros.h>
 #include <std_msgs/String.h>
 #include <std_msgs/Int16.h>
-#include "testbot/keynumber.h"
+#include "origarm_ros/keynumber.h"
 
 #include <iostream>
 #include <stdio.h>
@@ -15,7 +15,7 @@
 
 using namespace std;
 
-const char defualt_path[] = "/dev/input/by-path/platform-i8042-serio-0-event-kbd"; //AT Translated Set 2 keyboard
+const char defualt_path[] = "/dev/input/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.3:1.0-event-kbd"; //keyboard
 
 int keycode;
 int value[10];
@@ -49,7 +49,7 @@ int main(int argc, char** argv)
 	ros::init(argc, argv, "keyboard");
 	ros::NodeHandle n;
 	//ros::Publisher pub = n.advertise<std_msgs::Int16>("keyboard",100);
-	ros::Publisher pub = n.advertise<testbot::keynumber>("key_number",100);
+	ros::Publisher pub = n.advertise<origarm_ros::keynumber>("key_number",100);
 	ros::Rate r(100);
 	
 
@@ -87,7 +87,7 @@ int main(int argc, char** argv)
 		}
 
 		//std_msgs::Int16 keyboard;		
-		testbot::keynumber key_number;
+		origarm_ros::keynumber key_number;
 		for (int i = 0; i < 10; i++)
 		{
 			key_number.KEY_CODE[i] = value[i];
