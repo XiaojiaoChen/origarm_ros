5614ebf92402ab1d9d139c3f1107ea8c4f5df97b
lijing94123 <lijingtsinghua11@gmail.com>
2020-06-08 15:21:41 +0800

    Update joystick mode1 - mode 4

--- src/demo_picknplace.cpp
+++ src/demo_picknplace.cpp
@@ -11,7 +11,7 @@
 
 int tp = 1000;   //timestep
 int ts = 10000;  //time sleep at each step
-int repeat = 2; //cycles A->B, B->A
+int repeat = 2;  //cycles A->B, B->A
 int flag = 1;
 
 int mode_ = 4;
@@ -68,13 +68,15 @@ int main(int argc, char **argv)
 
 	ros::Publisher  pub1 = nh.advertise<origarm_ros::Command_Position>("Cmd_Position", 100);
 	ros::Publisher  pub2 = nh.advertise<origarm_ros::modenumber>("modenumber", 100);
-	ros::Publisher  pub3 = nh.advertise<origarm_ros::segnumber>("segnumber", 100);		
+	ros::Publisher  pub3 = nh.advertise<origarm_ros::segnumber>("segnumber", 100);
+	ros::Publisher  pub4 = nh.advertise<origarm_ros::Command_ABL>("Cmd_ABL_joy", 100);		
 
 	while (ros::ok())
 	{
 		origarm_ros::Command_Position Command_XYZ_demo;
 		origarm_ros::modenumber moden;
 		origarm_ros::segnumber segn;
+		origarm_ros::Command_ABL Command_ABL_demo;
 		
 		if (flag == 1)
 		{
@@ -85,6 +87,14 @@ int main(int argc, char **argv)
 				pub2.publish(moden);
 				segn.segmentNumber = segment_;
 				pub3.publish(segn);
+
+				for (int i = 0; i < 6; i++)
+				{
+					Command_ABL_demo.segment[i].A = 0;
+					Command_ABL_demo.segment[i].B = 0;
+					Command_ABL_demo.segment[i].L = length0;
+				}
+				pub4.publish(Command_ABL_demo);
 
 				demo_x = xorigin + i*(xa - xorigin)/(tp-1);
 				demo_y = yorigin + i*(ya - yorigin)/(tp-1);
@@ -114,7 +124,15 @@ int main(int argc, char **argv)
 					moden.modeNumber = mode_;
 					pub2.publish(moden);
 					segn.segmentNumber = segment_;
-					pub3.publish(segn);
+					pub3.publish(segn);	
+
+					for (int i = 0; i < 6; i++)
+					{
+						Command_ABL_demo.segment[i].A = 0;
+						Command_ABL_demo.segment[i].B = 0;
+						Command_ABL_demo.segment[i].L = length0;
+					}
+					pub4.publish(Command_ABL_demo);		
 
 					demo_x = xa + i*(xb - xa)/(tp-1);
 					demo_y = ya + i*(yb - ya)/(tp-1);
@@ -139,6 +157,14 @@ int main(int argc, char **argv)
 					pub2.publish(moden);
 					segn.segmentNumber = segment_;
 					pub3.publish(segn);
+
+					for (int i = 0; i < 6; i++)
+					{
+						Command_ABL_demo.segment[i].A = 0;
+						Command_ABL_demo.segment[i].B = 0;
+						Command_ABL_demo.segment[i].L = length0;
+					}
+					pub4.publish(Command_ABL_demo);
 
 					demo_x = xb + i*(xa - xb)/(tp-1);
 					demo_y = yb + i*(ya - yb)/(tp-1);
@@ -168,6 +194,14 @@ int main(int argc, char **argv)
 				pub2.publish(moden);
 				segn.segmentNumber = segment_;
 				pub3.publish(segn);
+
+				for (int i = 0; i < 6; i++)
+				{
+					Command_ABL_demo.segment[i].A = 0;
+					Command_ABL_demo.segment[i].B = 0;
+					Command_ABL_demo.segment[i].L = length0;
+				}
+				pub4.publish(Command_ABL_demo);
 
 				demo_x = xa + i*(xorigin - xa)/(tp-1);
 				demo_y = ya + i*(yorigin - ya)/(tp-1);
@@ -210,7 +244,7 @@ int main(int argc, char **argv)
 			printf("stay O, X: %f, Y: %f, Z: %f\r\n", demo_x, demo_y, demo_z);
 		}	
 				
-		ros::spinOnce();
+		//ros::spinOnce();
 		r.sleep();
 	}
 
