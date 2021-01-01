bd3bb21c01b3f86913b5c47f1d66ab5a8eafbf26
lijing94123 <lijing94123@github.com>
2021-01-01 17:17:02 +0800

    Update demo_abl_datacollection.cpp

--- src/demo_abl_datacollection.cpp
+++ src/demo_abl_datacollection.cpp
@@ -16,15 +16,10 @@
 using namespace std;
 
 const int mt = 1000; //1ms
-int ts = 1000*mt;    //time sleep at each point
+int ts = 2500*mt;    //time sleep at each point
 
-// int a1_step = 2; //0->amax
-// int b1_step = 2; //0-bmax
-// int l1_step = 2; //l0-lengthmax / l0->lengthmin
-// int a2_step = 4; //amin->amax
-// int b2_step = 2; //0-bmax
-// int l2_step = 2; //l0-lengthmax / l0->lengthmin
 int current_step = 0;
+int count_step = 0;
 
 float alpha1 =  0;
 float alpha2 =  0;
@@ -33,19 +28,31 @@ float beta2 = 0;
 float length1 = length0*3;
 float length2 = length0*3;
 
-// float alphamax =  0.35;
-// float alphamin = -0.35;
-// float betamax = M_PI/3;
-// float betamin = 0;
-// float lengthmin = 0.03;
-// float lengthmax = 0.08;
+// order: a2, b2, l2, a1, b1, l1
+vector<vector<int>> steps {
+	{      2,      1,       1,      1,      1,       1},
+	{      3,      3,       3,      3,      3,       3},
+	{      4,      1,       1,      1,      1,       1},
+	{      3,      3,       3,      3,      3,       3}};
 
-vector<float> p_start{-0.35*3,      0, 0.055*3,      0,      0, 0.055*3};
-vector<float> p_end  { 0.35*3, M_PI/3,  0.08*3, 0.35*3, M_PI/3,  0.08*3};
-vector<int> steps	 {      2,      2,       1,      1,      1,       1};
+vector<vector<float>> p_start_group{
+	{-0.35*3,      0, 0.055*3,      0,      0, 0.055*3},  //initial
+	{-0.35*3,      0, 0.055*3,      0,      0, 0.055*3},  //l0->lmax
+	{-0.35*3,      0, 0.055*3,      0,      0, 0.055*3},  //initial
+	{-0.35*3,      0, 0.055*3,      0,      0, 0.055*3}}; //l0->lmin
+
+vector<vector<float>> p_end_group{
+	{-0.35*3,     0, 0.055*3,      0,      0, 0.055*3},
+	{0.35*3, M_PI/3,  0.08*3, 0.35*3, M_PI/3,  0.08*3},
+	{-0.35*3,     0, 0.055*3,      0,      0, 0.055*3},
+	{0.35*3, M_PI/3,  0.03*3, 0.35*3, M_PI/3,  0.03*3}
+	};
+
 vector<vector<float>> trajectoryGroup;
+vector<vector<vector<float>>> trajectory_group;
 
 static int flag_start = 0;
+int trajgroup_no = 0;
 
 ros::Time ABLDataBeginTime;
 std::string getTimeString()
@@ -85,9 +92,9 @@ void keyCallback(const origarm_ros::keynumber &key)
 		// printf("KEY_B pressed!\r\n");
 		flag_start = 0;
 	}
-	else if (key.keycodePressed == KEY_D) // same as saving button
+	else if (key.keycodePressed == KEY_J) // same as saving button
 	{
-		// printf("KEY_D pressed!\r\n");
+		// printf("KEY_J pressed!\r\n");
 		flag_start = 1;
 	}	
 }
@@ -103,8 +110,11 @@ static vector<float> trajectoryGeneration(float ps, float pe, int tstep, int cyc
 		{
 			if (tstep == 1)
 			{
-				point_temp = ps;
-				trajectory.push_back(point_temp);
+				for (int j = 0; j < cycle; j++)
+				{
+					point_temp = ps;
+					trajectory.push_back(point_temp);
+				}				
 			}
 			else
 			{
@@ -124,30 +134,29 @@ static vector<vector<float>> TrajGroupGeneration(vector<float> & ps, vector<floa
 {	
 	int size_row = tstep.size();
 	int size_col = 1;
-	int cycle, totalstep;
+	int totalstep;
 	vector<float> traj_temp;
 	vector<vector<float>> traj_group;
-
+	int cycle = 1;	
+	
 	for (int i = 0; i < tstep.size(); i++)
 	{
 		size_col = size_col*tstep[i];
 	}
 	
 	for (int i = 0; i < size_row; i++)
-	{
-		int cycle = 1;
+	{		
 		if (i == 0)
-		{
-			cycle = 1;	
+		{				
 			totalstep = size_col/(cycle*tstep[i]);			
 		}
 		else
 		{
-			cycle = cycle*tstep[i-1];
+			cycle = cycle*tstep[i-1];			
 			totalstep = size_col/(cycle*tstep[i]);			
 		}
 
-		printf("totalstep: %d, size_col: %d, cycle: %d, tstep: %d\r\n", totalstep, size_col, cycle, tstep[i]);
+		// printf("totalstep: %d, size_col: %d, cycle: %d, tstep: %d\r\n", totalstep, size_col, cycle, tstep[i]);
 
 		traj_temp = trajectoryGeneration(ps[i], pe[i], tstep[i], cycle, totalstep);
 		traj_group.push_back(traj_temp);
@@ -159,34 +168,36 @@ static vector<vector<float>> TrajGroupGeneration(vector<float> & ps, vector<floa
 void writeABLCommand()
 {
 	if (flag_start)
-	{						
-		trajectoryGroup = TrajGroupGeneration(p_start, p_end, steps);
-
-		// for (int i = 0; i < trajectoryGroup.size(); i++)
-		// {
-		// 	for (int j = 0; j < trajectoryGroup[i].size(); j++)
-		// 	{
-		// 		printf("trajectoryGroup[%d][%d]: %.4f\r\n", i, j, trajectoryGroup[i][j]);
-		// 	}
-		// }
-
-		alpha2 = trajectoryGroup[0][current_step];
-		beta2 = trajectoryGroup[1][current_step];
-		length2 = trajectoryGroup[2][current_step];
-		alpha1 = trajectoryGroup[3][current_step];
-		beta1 = trajectoryGroup[4][current_step];
-		length1 = trajectoryGroup[5][current_step];
-
-		if (current_step < trajectoryGroup[0].size()-1)
+	{
+		if (trajgroup_no < trajectory_group.size() - 1)
 		{
-			current_step = current_step + 1; 
+			if (current_step >= trajectory_group[trajgroup_no][0].size())
+			{
+				current_step = 0;
+				trajgroup_no ++; 
+			}
+		}
+		else if (trajgroup_no == trajectory_group.size() - 1)
+		{
+			if (current_step >= trajectory_group[trajgroup_no][0].size())
+			{									
+				flag_start = 0;
+			}
 		}
 		else
 		{
-			current_step = 0;
+			flag_start = 0;
 		}
-							
-		usleep(ts);
+					
+		alpha2 = trajectory_group[trajgroup_no][0][current_step];
+		beta2 = trajectory_group[trajgroup_no][1][current_step];
+		length2 = trajectory_group[trajgroup_no][2][current_step];
+		alpha1 = trajectory_group[trajgroup_no][3][current_step];
+		beta1 = trajectory_group[trajgroup_no][4][current_step];
+		length1 = trajectory_group[trajgroup_no][5][current_step];
+
+		current_step ++;		
+		usleep(ts);																						
 	}
 	else
 	{
@@ -210,12 +221,30 @@ int main(int argc, char **argv)
 	ros::Publisher  pub1 = nh.advertise<origarm_ros::Command_ABL>("Cmd_ABL_joy", 100);	
 	origarm_ros::Command_ABL Command_ABL_demo;
 
+	//load parameters
+	for (int i = 0; i < p_start_group.size(); i++)
+	{
+		trajectoryGroup = TrajGroupGeneration(p_start_group[i], p_end_group[i], steps[i]);
+		trajectory_group.push_back(trajectoryGroup);
+	}
+
+	for (int i = 0; i < trajectory_group.size(); i++)
+	{
+		for (int j = 0; j < trajectory_group[i].size(); j++)
+		{
+			for (int k = 0; k < trajectory_group[i][j].size(); k++)
+			{
+				printf("trajectory_group[%d][%d][%d]: %.4f\r\n", i, j, k, trajectory_group[i][j][k]);
+			}			
+		}
+	}
+
 	while (ros::ok())
 	{				
 		writeABLCommand(); 
 		// printf("flag_start: %d, current_step: %d\r\n", flag_start, current_step);
 		// printf("alpha1: %.4f, beta1: %.4f, length1: %.4f, alpha2: %.4f, beta2: %.4f, length2: %.4f\r\n", 
-		// 				alpha1, beta1, length1, alpha2, beta2, length2);
+		// 		alpha1, beta1, length1, alpha2, beta2, length2);
 
 		for (int i = 0; i < 3; i++)
 		{
